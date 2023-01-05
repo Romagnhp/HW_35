a9de78a9cbeeee81cc831ea63bff8661d5e50592
Romagnhp <romagnhp@gmail.com>
2023-01-05 16:46:23 +0200

    rewrite json file

--- cgi-bin/python_scripts/myFormRegistration.py
+++ cgi-bin/python_scripts/myFormRegistration.py
@@ -11,11 +11,14 @@ login = formRegistation['login'].value
 password = formRegistation['password'].value
 repeatPasword = formRegistation['repeatPasword'].value
 
+tempJson = None
+
 if password == repeatPasword and re.findall("[0-9A-Za-z]", password) and re.findall("[0-9A-Za-z]", login):
     print("Регистрация успешно завершина")
 
-    with open('cgi-bin/usersList.json', 'a+') as file2:
+    with open('cgi-bin/usersList.json', 'r+') as file2:
         tempJson = json.load(file2)
+    with open('cgi-bin/usersList.json', 'w+') as file2:
         tempJson[login] = password
         json.dump(tempJson, file2)
 else:
